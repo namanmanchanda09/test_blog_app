8392f34e39ccb29796b5acc2582cf45898d9c7a5
namanmanchanda09 <naman.mohan09@outlook.com>
2018-11-10 23:30:30 +0530

    Changes made

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+
+  resources :articles
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+
